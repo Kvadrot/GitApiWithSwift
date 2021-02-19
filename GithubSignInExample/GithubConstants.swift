e6b16b30c943a216236d4c055331292e2b75090c
Kvadrot <send.me.your.maild@gmail.com>
2021-02-19 18:54:25 +0200

    test

---
+++ GithubSignInExample/GithubConstants.swift
@@ -0,0 +1,19 @@
+//
+//  GithubConstants.swift
+//  GithubSignInExample
+//
+//  Created by John Codeos on 12/14/19.
+//  Copyright © 2019 John Codeos. All rights reserved.
+//
+
+import Foundation
+
+struct GithubConstants {
+
+    static let CLIENT_ID = "0e02dd9f5eb38c4211c6"
+    static let CLIENT_SECRET = "122890a19ea2a495835956b4688c41a26c36eea4"
+    static let REDIRECT_URI = "https://github.com/johncodeos"
+    static let SCOPE = "read:user,user:email"
+    static let TOKENURL = "https://github.com/login/oauth/access_token"
+    
+}
